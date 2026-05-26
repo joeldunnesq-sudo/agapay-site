b4c8537e9dbb5d78891e97bf83a6e5c159a94aa3
joeldunnesq-sudo <joeldunnesq@gmail.com>
2026-05-26 14:10:06 -0500

    Delete functions/api directory

--- functions/api/parishes.js
+++
@@ -1,6 +0,0 @@
-import { json } from "../_shared/http.js";
-import { publicParishes } from "../_shared/parishes.js";
-
-export async function onRequestGet() {
-  return json({ parishes: publicParishes() });
-}
